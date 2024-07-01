e9f2e708755e6f0c0938379b454dbff3d7073caf
Justyn Spooner <justyn@dovu.io>
2024-07-01 13:38:07 +0100

    Server side bug

--- components/HederaWalletConnect.tsx
+++ components/HederaWalletConnect.tsx
@@ -1,26 +1,12 @@
 "use client";
 
-import type { HederaChainId } from "@hashgraph/hedera-wallet-connect";
-import { useEffect, useState } from "react";
+import { HederaChainId } from "@hashgraph/hedera-wallet-connect";
 
 export default function HederaWalletConnect() {
-  const [chainId, setChainId] = useState<HederaChainId>();
-
-  useEffect(() => {
-    const init = async () => {
-      const { HederaChainId } = await import(
-        "@hashgraph/hedera-wallet-connect"
-      );
-
-      setChainId(HederaChainId.Mainnet);
-    };
-    init();
-  }, []);
-
   return (
     <>
       <h2>Hedera Wallet Connect</h2>
-      <p>{chainId}</p>
+      <p>{HederaChainId.Mainnet}</p>
     </>
   );
 }
